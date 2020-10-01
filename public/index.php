4245fd74dcb689eac0743447d73d66b43336e3a6
milan-miscevic <milan.miscevic@gmail.com>
2020-10-01 17:49:41 +0200

    Add main script

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$definitions = require __DIR__ . '/../config/container.php';
+$builder = new ContainerBuilder();
+$builder->addDefinitions($definitions);
+$container = $builder->build();
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$app->get('/', EgcServices\Home\Action\Home::class);
+$app->get('/games', EgcServices\Game\Action\GameList::class);
+$app->post('/games/add', EgcServices\Game\Action\GameAdd::class);
+
+$app->run();
